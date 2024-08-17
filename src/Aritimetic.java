5a2acab8092ff5472f26532a45bc2c134300a12c
Gustavonn07 <gustavonepnog@gmail.com>
2024-08-17 09:46:35 -0300

    add: adicionando operações aritimeticas

---
+++ src/Aritimetic.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+
+public class Aritimetic {
+
+    public static void main(String[] args) {
+        // expression =  operands & operators
+        // operands   =  values, variables, numbers, quantity
+        // operators  =  + - * / %
+
+        byte friends = 10;
+        friends++;
+
+        System.out.println(friends);
+
+        // Se uma divisão for retornar float mesmo sendo um valor int:
+        byte batatas = 10;
+        float resultado = (float) batatas / 3;
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        System.out.println(df.format(resultado));
+    }
+}
